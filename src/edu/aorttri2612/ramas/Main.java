ccf42e980e5e9f2e6d2bc4f34d19dd0fff52289a
aorttri2612 <aorttri2612@g.educaand.es>
2025-11-19 21:35:08 +0100

    Estructura Eclipse + Main con Lotería (inicial)

---
+++ src/edu/aorttri2612/ramas/Main.java
@@ -0,0 +1,25 @@
+package edu.aorttri2612.ramas;
+
+import java.util.HashSet;
+import java.util.Random;
+
+public class Main {
+	public static void main(String[] args) {
+		HashSet<Integer> bolas = new HashSet<>();
+		Random r = new Random();
+
+		while (bolas.size() < 5) {
+			bolas.add(r.nextInt(49) + 1);
+		}
+
+		System.out.println("Números principales: " + bolas);
+
+		int complementario = r.nextInt(49) + 1;
+		System.out.println("Complementario: " + complementario);
+
+		int suma = bolas.stream().mapToInt(Integer::intValue).sum();
+		System.out.println("Suma de principales: " + suma);
+
+		System.out.println("¡Suerte en el sorteo, DAM!");
+	}
+}
